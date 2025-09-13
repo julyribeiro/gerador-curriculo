5745f093db8494f122a050775e04b552fd7c03f0
July Ribeiro <july.026@hotmail.com>
2025-09-13 20:36:47 -0300

    feat: inclusão de imagem de preview do site e update no botão de exportar PDF

--- src/components/Layout/PreviewSection.tsx
+++ src/components/Layout/PreviewSection.tsx
@@ -6,7 +6,7 @@ export default function PreviewSection() {
   return (
     <section>
       {/* Botão de exportação em PDF */}
-      <div className="py-3 px-193 bg-gray-50">
+      <div className="flex justify-end py-3 mr-5 bg-gray-50">
         <div className="w-50 rounded-md bg-blue-500 py-1 px-3 text-white hover:bg-blue-600"><ExportButton /></div>
       </div>
       <div id="cv-preview-section" className="flex-1 p-10 bg-white max-w-4xl mx-auto shadow-md rounded-md">
